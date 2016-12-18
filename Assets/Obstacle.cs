7225bb03aa5a0f147e3a2e7a6b2f0098b96eb6b7
Paweł Taborowski <paweltab@wp.pl>
2016-12-18 02:18:11 +0100

    Second chunk available. Hitting obstacles.

---
+++ Assets/Obstacle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void OnTriggerEnter(Collider col) {
+        //if (!entered) {
+        UnityStandardAssets.Characters.ThirdPerson.ThirdPersonUserControl.sinceCol = 0;
+            //entered = true;
+        //}
+    }
+}
--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/GenrateTrack.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/GenrateTrack.cs
@@ -4,17 +4,19 @@ using System.Collections;
 public class GenrateTrack : MonoBehaviour {
 	public GameObject[] chunks;
 	public const int MAX = 451;
-	public const float chunk_size_coef = 1f;
+	public const float chunk_size_coef = 10f;
 	private GameObject[] chunkBuffer = new GameObject[MAX];
-	private int index = 151;
-	private ulong total = 151;
+	private int index = 20;
+	private ulong total = 20;
 	// Use this for initialization
 	void Start () {
-		for (int i=0; i<index; i++) {
+		for (int i=0; i<(index-1); i++) {
 			chunkBuffer[i] = GameObject.Instantiate(chunks[0]);
 			chunkBuffer[i].transform.position = new Vector3(0f,0f,i*chunk_size_coef);
 		}
-	}
+        chunkBuffer[index-1] = GameObject.Instantiate(chunks[1]);
+        chunkBuffer[index-1].transform.position = new Vector3(0f, 0f, (index-1) * chunk_size_coef);
+    }
 	
 	// Update is called once per frame
 	void Update () {
